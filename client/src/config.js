3ec84ef64562f51629a9e1b8b33f35bb319c46df
Kusumika Barua <kusumikabarua@gmail.com>
2024-08-21 17:26:35 +0530

    Add. backend url updated

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,3 @@
- export const API_BASE_URL = "http://localhost:4000/api";
+ //export const API_BASE_URL = "http://localhost:4000/api";
+ export const API_BASE_URL = "https://musicapp-lk7l.onrender.com/api";
 
